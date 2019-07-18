d0134a0626ac9323c171466eccb779e8f0a30ab3
983708408@qq.com <983708408@qq.com>
2019-07-18 19:06:30 +0800

    optimize's

--- pkg/kafka_offset_tool.go
+++ pkg/kafka_offset_tool.go
@@ -16,9 +16,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/urfave/cli"
 	"log"
 	"math/rand"
 	"os"
@@ -26,13 +26,20 @@ import (
 	"sync"
 )
 
-type kafkaOpts struct {
-	action       string
+type kafkaOption struct {
+	client sarama.Client
+
 	brokers      string
-	groupFilter  string
-	topicFilter  string
-	offsetRanges string
 	kafkaVersion string
+
+	groupFilter    string
+	topicFilter    string
+	consumerFilter string
+
+	resetGroupId   string
+	resetTopic     string
+	resetPartition int
+	resetOffset    int64
 }
 
 type PartitionOffset struct {
@@ -43,75 +50,146 @@ type PartitionOffset struct {
 }
 
 var (
-	opts = kafkaOpts{}
+	opt = kafkaOption{}
 )
 
 /**
- * Parse usage options.
+ * Parse usage options.</br>
+ * See: https://github.com/urfave/cli#examples
  */
-func init() {
-	flag.StringVar(&opts.action, "action", "list-group",
-		"Kafka offset tool action.(default: list-group, option: list-group|list-topic|reset-offset)")
-	flag.StringVar(&opts.brokers, "brokers", "localhost:9092", "Kafka broker servers.(default: localhost:9092, e.g. host1:9092,host2:9092)")
-	flag.StringVar(&opts.groupFilter, "group-filter", "*", "Kafka group regular expression filter.(default: *)")
-	flag.StringVar(&opts.topicFilter, "topic-filter", "*", "Kafka topic regular expression filter.(default: *)")
-	flag.StringVar(&opts.offsetRanges, "offset-ranges", "*", "Kafka topic regular expression filter.(default: *)")
-	flag.StringVar(&opts.kafkaVersion, "kafka-version", "0.10.0.0", "Kafka support version.(default: 0.10.0.0, e.g. 0.9.0.0|1.0.0|1.1.1)")
-	flag.Parse()
-
-	// Printf usage, --help arg flag has built-in processed.
-	if flag.NFlag() <= 0 {
-		flag.Usage()
-		os.Exit(0)
-	}
-	// log.Printf("--- kafka options ---\n%s", opts)
-}
-
 func main() {
-	// Init connect.
-	client := connectServers()
-
-	// Extract topic partition offset of groups.
-	var groupTopicPartOffsets = analysisGroupTopicPartitionOffsets(client)
-	fmt.Print(groupTopicPartOffsets)
+	app := cli.NewApp()
+	app.Version = "v1.0.0"
+	app.Authors = []cli.Author{
+		{Name: "Wag sir", Email: "983708408@qq.com"},
+	}
+	app.Description = "KafkaOffsetTool is a lightweight tool for Kafka offset operation and maintenance."
+	app.Copyright = "(c) 1999 Serious Enterprise"
+	app.Commands = cli.Commands{
+		{
+			Name:        "list-group",
+			Usage:       "list-group [OPTION]...",
+			Description: "Get the group list.",
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "brokers,s", Value: "localhost:9092", Usage: "(default: localhost:9092) --brokers=127.0.0.1:9092",
+					Destination: &opt.brokers},
+				cli.StringFlag{Name: "version,v", Value: "0.10.0.0", Usage: "(default: 0.10.0.0) --version=0.10.0.0",
+					Destination: &opt.kafkaVersion},
+				cli.StringFlag{Name: "filter,f", Value: "*", Usage: "(default: *) --filter=myPrefix\\\\S*"},
+			},
+			Before: func(c *cli.Context) error {
+				return clientConnect()
+			},
+			Action: func(c *cli.Context) error {
+				//fmt.Fprintf(c.App.Writer, ":list-group--processing, %s", c.String("filter"))
+				return nil
+			},
+		},
+		{
+			Name:        "list-topic",
+			Usage:       "list-topic [OPTION]...",
+			Description: "Get the topic list.",
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "brokers,s", Value: "localhost:9092", Usage: "(default: localhost:9092) --brokers=127.0.0.1:9092",
+					Destination: &opt.brokers},
+				cli.StringFlag{Name: "version,v", Value: "0.10.0.0", Usage: "(default: 0.10.0.0) --version=0.10.0.0",
+					Destination: &opt.kafkaVersion},
+				cli.StringFlag{Name: "filter,f", Value: "*", Usage: "(default: *) --filter=myPrefix\\\\S*"},
+			},
+			Before: func(c *cli.Context) error {
+				return clientConnect()
+			},
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:        "list-consumer",
+			Usage:       "list-consumer [OPTION]...",
+			Description: "Get the consumer list.",
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "brokers,s", Value: "localhost:9092", Usage: "(default: localhost:9092) --brokers=127.0.0.1:9092",
+					Destination: &opt.brokers},
+				cli.StringFlag{Name: "version,v", Value: "0.10.0.0", Usage: "(default: 0.10.0.0) --version=0.10.0.0",
+					Destination: &opt.kafkaVersion},
+				cli.StringFlag{Name: "filter,f", Value: "*", Usage: "(default: *) --filter=myPrefix\\\\S*"},
+				cli.StringFlag{Name: "type,t", Value: "*", Usage: "(default: *) --type=zk|kf"},
+			},
+			Before: func(c *cli.Context) error {
+				return clientConnect()
+			},
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:        "reset-offset",
+			Usage:       "reset-offset [OPTION]...",
+			Description: "Reset the offset of the specified grouping topic partition.",
+			Flags: []cli.Flag{
+				cli.StringFlag{Name: "brokers,s", Value: "localhost:9092", Usage: "(default: localhost:9092) --brokers=127.0.0.1:9092",
+					Destination: &opt.brokers},
+				cli.StringFlag{Name: "version,v", Value: "0.10.0.0", Usage: "(default: 0.10.0.0) --version=0.10.0.0",
+					Destination: &opt.kafkaVersion},
+				cli.StringFlag{Name: "group,g", Usage: "--group=myGroup", Destination: &opt.resetGroupId},
+				cli.StringFlag{Name: "topic,t", Usage: "--topic=myTopic", Destination: &opt.resetTopic},
+				cli.IntFlag{Name: "partition,p", Usage: "--partition=0", Destination: &opt.resetPartition},
+				cli.Int64Flag{Name: "offset,f", Usage: "--partition=0", Destination: &opt.resetOffset},
+			},
+			Before: func(c *cli.Context) error {
+				return clientConnect()
+			},
+			Action: func(c *cli.Context) error {
+				resetOffset()
+				return nil
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Printf("See 'kafkaOffsetTool --help'. %s", err.Error())
+	}
 }
 
 // Connect to kafka broker servers.
-func connectServers() sarama.Client {
-	log.Printf("Connect to kafka servers...")
+func clientConnect() error {
+	if opt.client == nil {
+		log.Printf("Connect to kafka servers...")
 
-	// Init configuration.
-	config := sarama.NewConfig()
-	config.ClientID = fmt.Sprintf("kafkaOffsetTool-%d", rand.Int())
-	if kafkaVer, e1 := sarama.ParseKafkaVersion(opts.kafkaVersion); e1 == nil {
-		config.Version = kafkaVer
-	} else {
-		log.Panicf("Unrecognizable kafka version. %s", e1)
-	}
+		// Init configuration.
+		config := sarama.NewConfig()
+		config.ClientID = fmt.Sprintf("kafkaOffsetTool-%d", rand.Int())
+		if kafkaVer, e1 := sarama.ParseKafkaVersion(opt.kafkaVersion); e1 == nil {
+			config.Version = kafkaVer
+		} else {
+			log.Panicf("Unrecognizable kafka version. %s", e1)
+		}
 
-	// Create kafka client.
-	var client, e2 = sarama.NewClient(strings.Split(opts.brokers, ","), config)
-	if e2 != nil {
-		log.Panicf("Unable connect kafka brokers. %s", e2)
+		// Create kafka client.
+		var client, e2 = sarama.NewClient(strings.Split(opt.brokers, ","), config)
+		if e2 != nil {
+			log.Panicf("Unable connect kafka brokers. %s", e2)
+		}
+		// defer client.Close()
+		opt.client = client
+		return e2
 	}
-	// defer client.Close()
-	return client
+	return nil
 }
 
-// Extract and analysis topic partition offsets of groups.
-func analysisGroupTopicPartitionOffsets(client sarama.Client) map[string]map[string]map[int32]PartitionOffset {
+// Analysis and extract topic partition offsets of groups.
+func analysisGroupTopicPartitionOffsets() map[string]map[string]map[int32]PartitionOffset {
 	// Get partition offsets of topics.
-	var topicPartOffsets = topicPartitionOffsets(client)
+	var topicPartOffsets = topicPartitionOffsets()
 	log.Printf("Analysis topic partition offset relation basis on groups...")
 
 	// Get group offsets info.
 	groupTopicPartOffsets := make(map[string]map[string]map[int32]PartitionOffset)
-	if len(client.Brokers()) <= 0 {
+	if len(opt.client.Brokers()) <= 0 {
 		log.Panicf("Cannot get topic group information, no valid broker.")
 	}
 
-	for _, broker := range client.Brokers() {
-		if err := broker.Open(client.Config()); err != nil && err != sarama.ErrAlreadyConnected {
+	for _, broker := range opt.client.Brokers() {
+		if err := broker.Open(opt.client.Config()); err != nil && err != sarama.ErrAlreadyConnected {
 			log.Panicf("Cannot connect to brokerID: %d, %s", broker.ID(), err)
 		}
 
@@ -182,10 +260,10 @@ func analysisGroupTopicPartitionOffsets(client sarama.Client) map[string]map[str
 }
 
 // Get partitions offsets of topics.
-func topicPartitionOffsets(client sarama.Client) map[string]map[int32]PartitionOffset {
+func topicPartitionOffsets() map[string]map[int32]PartitionOffset {
 	log.Printf("Fetching metadata of the topic partitions infor...")
 
-	var topics, err = client.Topics()
+	var topics, err = opt.client.Topics()
 	if err != nil {
 		log.Panicf("Cannot get topics. %s", err)
 	}
@@ -202,7 +280,7 @@ func topicPartitionOffsets(client sarama.Client) map[string]map[int32]PartitionO
 		go func(topic string) {
 			wg.Add(1)
 			defer wg.Done()
-			partitions, err := client.Partitions(topic)
+			partitions, err := opt.client.Partitions(topic)
 			if err != nil {
 				log.Panicf("Cannot get partitions of topic: %s, %s", topic, err)
 			}
@@ -218,7 +296,7 @@ func topicPartitionOffsets(client sarama.Client) map[string]map[int32]PartitionO
 				mu.Unlock()
 
 				// Current offset.
-				currentOffset, err := client.GetOffset(topic, partition, sarama.OffsetNewest)
+				currentOffset, err := opt.client.GetOffset(topic, partition, sarama.OffsetNewest)
 				if err != nil {
 					log.Panicf("Cannot get current offset of topic: %s, partition: %d, %s", topic, partition, err)
 				} else {
@@ -228,7 +306,7 @@ func topicPartitionOffsets(client sarama.Client) map[string]map[int32]PartitionO
 				}
 
 				// Oldest offset.
-				oldestOffset, err := client.GetOffset(topic, partition, sarama.OffsetOldest)
+				oldestOffset, err := opt.client.GetOffset(topic, partition, sarama.OffsetOldest)
 				if err != nil {
 					log.Panicf("Cannot get current oldest offset of topic: %s, partition: %d, %s", topic, partition, err)
 				} else {
@@ -245,11 +323,12 @@ func topicPartitionOffsets(client sarama.Client) map[string]map[int32]PartitionO
 
 // Reset topic group partitions offset.
 // See: https://github.com/Shopify/sarama/blob/master/offset_manager_test.go#L228
-func resetOffset(client sarama.Client, offset int64, topic string, groupId string, partition int32) {
-	var offsetManager, _ = sarama.NewOffsetManagerFromClient(groupId, client)
+func resetOffset() {
+	var offsetManager, _ = sarama.NewOffsetManagerFromClient(opt.resetGroupId, opt.client)
 	fmt.Print(offsetManager)
-	var pom, _ = offsetManager.ManagePartition(topic, partition)
-	pom.ResetOffset(offset, "modified_meta")
+	var pom, _ = offsetManager.ManagePartition(opt.resetTopic, int32(opt.resetPartition))
+	pom.ResetOffset(opt.resetOffset, "modified_meta")
 
-	log.Printf("Reseted offset: %d, topic: %s, group: %s, partition: %d", offset, topic, groupId, partition)
+	log.Printf("Reseted offset: %d, topic: %s, group: %s, partition: %d",
+		opt.resetOffset, opt.resetTopic, opt.resetGroupId, opt.resetPartition)
 }
