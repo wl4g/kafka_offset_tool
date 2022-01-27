ad5988e5423014d8703210731f3c684489978a13
wl4g <983708408@qq.com>
2022-01-27 22:20:12 +0800

    fix:

--- pkg/common/maps_test.go
+++ pkg/common/maps_test.go
@@ -7,10 +7,19 @@ import (
 
 func TestToOrderedKeys(t *testing.T) {
 	fmt.Printf("Testing %s starting ...\n", "TestToOrderedKeys")
-	mymap := make(map[string]int)
-	mymap["a1"] = 11
-	mymap["a2"] = 22
-	mymap["b1"] = 33
-	mymap["b1"] = 44
-	fmt.Println(mymap)
+	mymap := make(map[string]string)
+	mymap["a3"] = "Jack"
+	mymap["a1"] = "Washington"
+	mymap["a2"] = "Marry"
+	mymap["b1"] = "Saxon"
+	mymap["c1"] = "Michael Jackson"
+	mymap["d1"] = "Da Vinci"
+	fmt.Println("---------- Ordered foreach map ------------")
+	keys, err := ToOrderedKeys(mymap)
+	if err != nil {
+		panic("Error to orderedKeys")
+	}
+	for _, key := range keys {
+		fmt.Printf("%s => %s\n", key, mymap[key])
+	}
 }
